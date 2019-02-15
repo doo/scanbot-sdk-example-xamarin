e0c60af0de2bd2d1d135f05ae4705900857404fb
Dmytro Makarenko <dmytro@scanbot.io>
2019-02-15 12:41:33 +0100

    Added business cards demo

---
+++ iOS/ViewControllers/Business Cards/BusinessCardDetailDemoViewController.cs
@@ -0,0 +1,42 @@
+﻿using UIKit;
+using System;
+
+namespace scanbotsdkexamplexamarin.iOS
+{
+    public partial class BusinessCardDetailDemoViewController : UIViewController
+    {
+        private BusinessCard _businessCard;
+        public BusinessCard BusinessCard
+        {
+            get
+            {
+                return _businessCard;
+            }
+
+            set
+            {
+                _businessCard = value;
+                UpdateBusinessCard();
+            }
+        }
+
+        public BusinessCardDetailDemoViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            UpdateBusinessCard();
+        }
+
+        private void UpdateBusinessCard()
+        {
+            if (IsViewLoaded)
+            {
+                imageView.Image = BusinessCard.Image;
+                textView.Text = BusinessCard.RecognizedText;
+            }
+        }
+    }
+}
---
+++ iOS/ViewControllers/Business Cards/BusinessCardsDemoViewController.designer.cs
@@ -0,0 +1,60 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace scanbotsdkexamplexamarin.iOS
+{
+    [Register ("BusinessCardsDemoViewController")]
+    partial class BusinessCardsDemoViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton closeCollectionContainerButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.NSLayoutConstraint collectionContainerHeightConstraint { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UICollectionView collectionView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView loadingView { get; set; }
+
+        [Action ("closeCollectionContainerTapped:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void CloseCollectionContainerTapped (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (closeCollectionContainerButton != null) {
+                closeCollectionContainerButton.Dispose ();
+                closeCollectionContainerButton = null;
+            }
+
+            if (collectionContainerHeightConstraint != null) {
+                collectionContainerHeightConstraint.Dispose ();
+                collectionContainerHeightConstraint = null;
+            }
+
+            if (collectionView != null) {
+                collectionView.Dispose ();
+                collectionView = null;
+            }
+
+            if (loadingView != null) {
+                loadingView.Dispose ();
+                loadingView = null;
+            }
+        }
+    }
+}
